16882b77dbba4afa56cbe05775a3cd6a4574cf1c
Zack Shrout <zshrout@gmail.com>
2021-11-21 07:16:37 -0600

    Changed file structure.

---
+++ Engine/Platform/Window.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "CommonHeaders.h"
+
+namespace primal::platform {
+
+DEFINE_TYPED_ID(window_id);
+
+class window
+{
+public:
+    constexpr explicit window(window_id id) : _id{ id } {}
+    constexpr window() = default;
+    constexpr window_id get_id() const { return _id; }
+    constexpr bool is_valid() const { return id::is_valid(_id); }
+
+    void set_fullscreen(bool is_fullscreen) const;
+    bool is_fullscreen() const;
+    void* handle() const;
+    void set_caption(const wchar_t* caption) const;
+    math::u32v4 size() const;
+    void resize(u32 width, u32 height) const;
+    u32 width() const;
+    u32 height() const;
+    bool is_closed() const;
+#ifdef __linux__
+    void close();
+#endif // __linux__
+private:
+    window_id _id{ id::invalid_id };
+};
+}
