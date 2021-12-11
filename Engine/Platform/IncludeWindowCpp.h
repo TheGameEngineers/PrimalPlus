fa80e40282ec15c1b537b9b9b9ef6dd93ffb3382
Arash Khatami <arash.khatami80@gmail.com>
2021-12-11 16:23:25 +0100

    Suggestion for including window.cpp at the end of platformXX cpp files.

---
+++ Engine/Platform/IncludeWindowCpp.h
@@ -0,0 +1,6 @@
+// Copyright (c) Arash Khatami
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+#pragma once
+#define INCLUDE_WINDOW_CPP 1
+#include "Window.cpp"
+#undef INCLUDE_WINDOW_CPP
--- Engine/Platform/PlatformTypes.h
+++ Engine/Platform/PlatformTypes.h
@@ -30,18 +30,18 @@ struct window_init_info
 #include <stdlib.h>
 
 namespace primal::platform {
-	
+
 using window_handle = Window*;
 
 struct window_init_info
 {
-	void*			callback{ nullptr };
-	window_handle	parent{ nullptr };
-	const wchar_t*	caption{ nullptr };
-	s32				left{ 0 };
-	s32				top{ 0 };
-	s32				width{ 1920 };
-	s32				height{ 1080 };
+    void*			callback{ nullptr };
+    window_handle	parent{ nullptr };
+    const wchar_t*	caption{ nullptr };
+    s32				left{ 0 };
+    s32				top{ 0 };
+    s32				width{ 1920 };
+    s32				height{ 1080 };
 };
 }
 #endif // __linux__
