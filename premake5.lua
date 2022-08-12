c16c09d477770581bb72dcd7fa831d4ccfb9a15c
Zack Shrout <zshrout@gmail.com>
2022-08-12 13:17:17 -0500

    Added Linux GCC support to Premake script

--- premake5.lua
+++ premake5.lua
@@ -2,10 +2,15 @@ workspace "Primal"
     configurations { "Debug", "Release", "DebugEditor", "ReleaseEditor" }
     platforms "x64"
     architecture "x64"
-    startproject "PrimalEditor"
+
+    if _TARGET_OS == "windows" then
+        startproject "PrimalEditor"
+    else
+        startproject "EngineTest"
+    end
 
     filter "configurations:Debug"
-        defines { "DEBUG" }
+        defines "DEBUG"
         symbols "On"
     
     filter "configurations:DebugEditor"
@@ -13,7 +18,7 @@ workspace "Primal"
         symbols "On"
     
     filter "configurations:Release"
-        defines { "NDEBUG" }
+        defines "NDEBUG"
         optimize "On"
         omitframepointer "On"
     
@@ -21,33 +26,45 @@ workspace "Primal"
         defines { "NDEBUG" , "USE_WITH_EDITOR" }
         optimize "On"
         omitframepointer "On"
-    filter{}
 
-   outputdir = "$(SolutionDir)$(Platform)/$(Configuration)/"
-   intermediatesdir="$(Platform)/$(Configuration)/"
+    if _TARGET_OS == "linux" then
+        toolset "gcc"
+    end
 
--- This should only build in DebugEditor and ReleaseEditor configurations
-project "ContentTools" 
-    location "ContentTools"
-    kind "SharedLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "Off"
-    targetname "$(ProjectName)"
-    targetdir (outputdir)
-    objdir (intermediatesdir)
-    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
-    includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common", "C:/Program Files/Autodesk/FBX/FBX SDK/2020.2/include" }
-    rtti "Off"
-    floatingpoint "Fast"
-    conformancemode "On"
-    exceptionhandling "Off"
-    warnings "Extra"
-    removeconfigurations { "Release", "Debug" }
+    -- If this is generated for VisualStudio, use their own path macros. If not, use Premake's wildcards
+    if _ACTION == "vs2022" then
+        outputdir = "$(SolutionDir)$(Platform)/$(Configuration)/"
+        intermediatesdir="$(Platform)/$(Configuration)/"
+    else
+        outputdir = "%{wks.location}/%{cfg.platform}/%{cfg.buildcfg}"
+        intermediatesdir = "%{cfg.platform}/%{cfg.buildcfg}"
+    end
 
-    filter "system:windows"
+-- This should only build in DebugEditor and ReleaseEditor configurations, and therefore only build in
+-- the Windows environment
+if _TARGET_OS == "windows" then
+    project "ContentTools" 
+        location "ContentTools"
+        kind "SharedLib"
+        language "C++"
+        cppdialect "C++17"
+        staticruntime "Off"
+        targetname "$(ProjectName)"
+        targetdir (outputdir)
+        objdir (intermediatesdir)
+        files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
+        includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common", "C:/Program Files/Autodesk/FBX/FBX SDK/2020.2/include" }
+        rtti "Off"
+        floatingpoint "Fast"
+        conformancemode "On"
+        exceptionhandling "Off"
+        warnings "Extra"
+        removeconfigurations { "Release", "Debug" }
+
+        -- filter "system:windows"
         systemversion "latest"
         defines { "CONTENTTOOLS_EXPORTS", "_WINDOWS", "_USRDLL" }
+end
 
 -- This should build in all configurations
 project "Engine"
@@ -56,45 +73,51 @@ project "Engine"
     language "C++"
     cppdialect "C++17"
     staticruntime "Off"
-    targetname "$(ProjectName)"
+    if _TARGET_OS == "windows" then
+        targetname "$(ProjectName)"
+        includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common", "$(VULKAN_SDK)/Include" }
+        libdirs "$(VULKAN_SDK)/Lib"
+        systemversion "latest"
+        defines "_LIB"
+    else
+        targetname "%{prj.name}"
+        includedirs { "%{wks.location}/Engine", "%{wks.location}/Engine/Common" }
+    end
     targetdir (outputdir)
     objdir (intermediatesdir)
     files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
-    includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common", "$(VULKAN_SDK)/Include" }
-    libdirs "$(VULKAN_SDK)/Lib"
     rtti "Off"
     floatingpoint "Fast"
     conformancemode "On"
     exceptionhandling "Off"
     warnings "Extra"
 
-    filter "system:windows"
-        systemversion "latest"
-        defines { "_LIB" }
+-- This should only build in DebugEditor and ReleaseEditor configurations, and therefore only build in
+-- the Windows environment
+if _TARGET_OS == "windows" then
+    project "EngineDLL"
+        location "EngineDLL"
+        kind "SharedLib"
+        language "C++"
+        cppdialect "C++17"
+        staticruntime "Off"
+        targetname "$(ProjectName)"
+        targetdir (outputdir)
+        objdir (intermediatesdir)
+        files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
+        includedirs {"$(SolutionDir)Engine", "$(SolutionDir)Engine/Common"}
+        rtti "Off"	
+        floatingpoint "Fast"
+        conformancemode "On"
+        exceptionhandling "Off"
+        warnings "Extra"
+        dependson "Engine"
+        removeconfigurations { "Release", "Debug" }
 
--- This should only build in DebugEditor and ReleaseEditor configurations
-project "EngineDLL"
-    location "EngineDLL"
-    kind "SharedLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "Off"
-    targetname "$(ProjectName)"
-    targetdir (outputdir)
-    objdir (intermediatesdir)
-    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
-    includedirs {"$(SolutionDir)Engine", "$(SolutionDir)Engine/Common"}
-    rtti "Off"	
-    floatingpoint "Fast"
-    conformancemode "On"
-    exceptionhandling "Off"
-    warnings "Extra"
-    dependson "Engine"
-    removeconfigurations { "Release", "Debug" }
-
-    filter "system:windows"
+        -- filter "system:windows"
         systemversion "latest"
         defines { "ENGINEDLL_EXPORTS", "_WINDOWS", "_USRDLL" }
+end
 
 -- This should only build in Debug and Release configurations
 project "EngineTest"
@@ -103,12 +126,20 @@ project "EngineTest"
     language "C++"
     cppdialect "C++17"
     staticruntime "Off"
-    targetname "$(ProjectName)"
+    if _TARGET_OS == "windows" then
+        targetname "$(ProjectName)"
+        includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common" }
+        libdirs "$(OutDir)"
+        systemversion "latest"
+        defines "_CONSOLE"
+    else
+        targetname "%{prj.name}"
+        includedirs { "%{wks.location}/Engine", "%{wks.location}/Engine/Common" }
+        libdirs (outputdir)
+    end
     targetdir (outputdir)
     objdir (intermediatesdir)
     files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
-    includedirs { "$(SolutionDir)Engine", "$(SolutionDir)Engine/Common" }
-    libdirs "$(OutDir)"
     rtti "Off"	
     floatingpoint "Fast"
     conformancemode "On"
@@ -118,18 +149,19 @@ project "EngineTest"
     removeconfigurations { "ReleaseEditor", "DebugEditor" }
 
     filter "system:windows"
-        systemversion "latest"
-        defines "_CONSOLE"
         prebuildcommands {"powershell -ExecutionPolicy Bypass -File $(SolutionDir)GetDXC.ps1 $(SolutionDir)packages\\DirectXShaderCompiler",
                           "xcopy /Y /D $(SolutionDir)packages\\DirectXShaderCompiler\\bin\\x64\\dxcompiler.dll $(OutDir)",
                           "xcopy /Y /D $(SolutionDir)packages\\DirectXShaderCompiler\\bin\\x64\\dxil.dll $(OutDir)" }
         prebuildmessage "If packages\\DirectXShaderCompiler\\ folder doesn't exist or is empty then download the latest release of DXC"
 
--- This should only build in DebugEditor and ReleaseEditor configurations
-project "PrimalEditor"
-    location "PrimalEditor"
-    kind "WindowedApp"
-    language "C#"
-    dotnetframework "net6.0" -- net6.0 for .NET 6.0, netcoreapp3.1 for .NET Core 3.1
-    targetdir (outputdir)
-    removeconfigurations { "Release", "Debug" }
+-- This should only build in DebugEditor and ReleaseEditor configurations, and therefore only build in
+-- the Windows environment
+if _TARGET_OS == "windows" then
+    project "PrimalEditor"
+        location "PrimalEditor"
+        kind "WindowedApp"
+        language "C#"
+        dotnetframework "net6.0" -- net6.0 for .NET 6.0, netcoreapp3.1 for .NET Core 3.1
+        targetdir (outputdir)
+        removeconfigurations { "Release", "Debug" }
+end
