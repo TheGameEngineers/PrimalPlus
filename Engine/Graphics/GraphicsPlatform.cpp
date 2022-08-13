b03ad36983dedf4ec679928d6881035455cf5f32
Arash Khatami <arash.khatami80@gmail.com>
2022-08-13 12:08:16 +0200

    typo

--- Engine/Graphics/GraphicsPlatform.cpp
+++ Engine/Graphics/GraphicsPlatform.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) Arash Khatami
-// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+// Copyright (c) Contributors of Primal+
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.#pragma once
 #include "GraphicsPlatformInterface.h"
 #include "Direct3D12\D3D12Interface.h"
 #include "Vulkan\VulkanInterface.h"
